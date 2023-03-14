ab87bda05427cf3aa68b4405cfb9c98271108380
Michael Allen <chaseallenskis@gmail.com>
2023-03-14 12:35:41 -0600

    Added environments and angular/fire to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { HomePageComponent } from './home-page/home-page.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
 
 
 
@@ -19,9 +25,16 @@ import { HomePageComponent } from './home-page/home-page.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    SharedModule
+    SharedModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAnalytics(() => getAnalytics()),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideFunctions(() => getFunctions())
   ],
-  providers: [],
+  providers: [
+    ScreenTrackingService,UserTrackingService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'kanban-angularfire',
+    appId: '1:572527375733:web:c9077e8b9b24483d290f3c',
+    storageBucket: 'kanban-angularfire.appspot.com',
+    apiKey: 'AIzaSyA74H6o348uIlCpfJJ1HTpIFifVk_wdB94',
+    authDomain: 'kanban-angularfire.firebaseapp.com',
+    messagingSenderId: '572527375733',
+    measurementId: 'G-H6EE9GV66N',
+  },};
