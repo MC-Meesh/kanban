01bad12a6175400ac3582a41b6f84fed8b9ccd02
Michael Allen <chaseallenskis@gmail.com>
2023-03-15 20:03:25 -0600

    logoff

--- src/app/user/email-login/email-login.component.ts
+++ src/app/user/email-login/email-login.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators
+} from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-email-login',
@@ -6,5 +12,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./email-login.component.scss']
 })
 export class EmailLoginComponent {
+  form: FormGroup;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
