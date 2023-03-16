71abbfa1ae839b8e5633bbb1348fa3a558e6ece4
Michael Allen <chaseallenskis@gmail.com>
2023-03-15 22:52:33 -0600

    Added auth guard to home page for testing. Remobeing home guard after at next commit

--- src/app/user/auth.guard.ts
+++ src/app/user/auth.guard.ts
@@ -2,21 +2,24 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { SnackService } from '../services/snack.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private afAuth: AngularFireAuth, ){}
+  constructor(private afAuth: AngularFireAuth, private snack: SnackService){}
 
   async canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean>{
 
     const user = await this.afAuth.currentUser;
-    const isLoggedin = !!user;
+    const isLoggedin = !!user; //cast null|true to boolean (false|true) with !!
+    if (!isLoggedin){
+      this.snack.authError();
+    }
 
-    return isLoggedin;
-      
+    return isLoggedin;   
   }
   
 }
