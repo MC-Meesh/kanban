12282198cd3be3c21da5acb582e1aa695cd034ac
Michael Allen <chaseallenskis@gmail.com>
2023-03-15 22:20:00 -0600

    Added email authentication, the customization for the form is still not correct but its functional

--- src/app/user/email-login/email-login.component.ts
+++ src/app/user/email-login/email-login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {Form, FormBuilder,FormGroup,Validators} from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
@@ -9,80 +13,81 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 })
 export class EmailLoginComponent implements OnInit {
   form: FormGroup;
+
   type: 'login' | 'signup' | 'reset' = 'signup';
   loading = false;
-  serverMessage: string;
 
-  constructor(private afAuth: AngularFireAuth, private fb: FormBuilder){}
+  serverMessage: any;
 
-  ngOnInit(): void {
-      this.form = this.fb.group({
-        email: ['', [Validators.required, Validators.email]],
-        password:[
-          '',
-          [Validators.minLength(6), Validators.required]
-        ],
-        passwordConfirm: ['', []]
-      })
+  constructor(private afAuth: AngularFireAuth, private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [Validators.minLength(6), Validators.required]
+      ],
+      passwordConfirm: ['', []]
+    });
   }
 
-  changeType(val: any){
+  changeType(val: any) {
     this.type = val;
   }
 
-
-  get isLogin(){
+  get isLogin() {
     return this.type === 'login';
   }
 
-  get isSignup(){
+  get isSignup() {
     return this.type === 'signup';
   }
 
-  get isPasswordReset(){
+  get isPasswordReset() {
     return this.type === 'reset';
   }
 
-  get email(){
+  get email() {
     return this.form.get('email');
   }
-
-  get password(){
+  get password() {
     return this.form.get('password');
   }
 
-  get passwordConfirm(){
+  get passwordConfirm() {
     return this.form.get('passwordConfirm');
   }
 
-  get passwwordDoesMatch() {
-    if (this.type !== 'signup'){
+  get passwordDoesMatch() {
+    if (this.type !== 'signup') {
       return true;
     } else {
       return this.password?.value === this.passwordConfirm?.value;
     }
   }
 
-  async onSubmit(){
-    
+  async onSubmit() {
+    this.loading = true;
+
+    const email = this.email?.value;
+    const password = this.password?.value;
+
+    try {
+      if (this.isLogin) {
+        await this.afAuth.signInWithEmailAndPassword(email, password);
+      }
+      if (this.isSignup) {
+        await this.afAuth.createUserWithEmailAndPassword(email, password);
+      }
+      if (this.isPasswordReset) {
+        await this.afAuth.sendPasswordResetEmail(email);
+        this.serverMessage = 'Check your email';
+      }
+    } catch (err) {
+      this.serverMessage = err;
+    }
+
+    this.loading = false;
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
