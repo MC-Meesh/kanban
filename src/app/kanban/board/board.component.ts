464817a0f9b8377df3c787605def9da823ea2ae5
Michael Allen <chaseallenskis@gmail.com>
2023-03-20 16:55:55 -0600

    Added dialog feature for adding tasks to each board. Onclick is not funcitonal

--- src/app/kanban/board/board.component.ts
+++ src/app/kanban/board/board.component.ts
@@ -1,6 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { BoardService } from '../board.service';
+import { Task } from '../board.model';
+import { MatDialog } from '@angular/material/dialog';
+import { TaskDialogComponent } from '../dialogs/task-dialog.component';
+
 
 @Component({
   selector: 'app-board',
@@ -8,6 +12,7 @@ import { BoardService } from '../board.service';
   styleUrls: ['./board.component.scss']
 })
 export class BoardComponent {
+  dialog:MatDialog;
   @Input() board:any;
 
   constructor(private boardService: BoardService) {}
@@ -16,4 +21,32 @@ export class BoardComponent {
     moveItemInArray(this.board.tasks, event.previousIndex, event.currentIndex);
     this.boardService.updateTasks(this.board.id, this.board.tasks);
   }
+
+
+  
+  openDialog(task?: Task, idx?: number): void {
+    const newTask = { label: 'purple' };
+    const dialogRef = this.dialog.open(TaskDialogComponent, {
+      width: '500px',
+      data: task
+        ? { task: { ...task }, isNew: false, boardId: this.board.id, idx }
+        : { task: newTask, isNew: true }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        if (result.isNew) {
+          this.boardService.updateTasks(this.board.id, [
+            ...this.board.tasks,
+            result.task
+          ]);
+        } else {
+          const update = this.board.tasks;
+          update.splice(result.idx, 1, result.task);
+          this.boardService.updateTasks(this.board.id, this.board.tasks);
+        }
+      }
+    });
+  }
+
 }
